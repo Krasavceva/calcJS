1f2ccb528c9091f11a6defe3d594f501df0eb699
Krasavceva <krasavceva.va@gmail.com>
2022-05-16 17:10:10 +0300

    ready

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,35 @@
 
+function summ() {
+    let a = document.getElementById('firstNumber').value;
+    let b = document.getElementById('secondNumber').value;
+    let result = +a + +b;
+    document.getElementById('result').value = result;
+}
 
+function subt() {
+    let a = document.getElementById('firstNumber').value;
+    let b = document.getElementById('secondNumber').value;
+    let result = +a - +b;
+    document.getElementById('result').value = result;
+}
+
+function multip() {
+    let a = document.getElementById('firstNumber').value;
+    let b = document.getElementById('secondNumber').value;
+    let result = +a * +b;
+    document.getElementById('result').value = result;
+}
+
+function divis() {
+    let a = document.getElementById('firstNumber').value;
+    let b = document.getElementById('secondNumber').value;
+    let result = +a / +b;
+    document.getElementById('result').value = result;
+}
+
+function getReset() {
+    document.querySelector("#firstNumber").value="";
+    document.querySelector("#secondNumber").value="";
+    document.querySelector("#result").value="";
+}
 
